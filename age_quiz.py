c1d65887b05b5adedccc15098ffd2142788420dc
Steven Blundell <Steven.blundell1985@gmail.com>
2023-12-30 15:06:51 +0000

    functions

---
+++ age_quiz.py
@@ -0,0 +1,19 @@
+# need to establish the persons aged
+
+# using the input function i can get this information 
+age = int(input("please provide us with your age : "))
+
+# if the person inputs any of the relevent ages they will receive the following responses 
+if age < 13:
+    print("you qualify for the kiddies discount")
+elif age >= 100:
+    print("Sorry, you're Dead")
+elif age > 65:
+    print("Enjoy your retirement!")
+elif age > 40:
+    print("you've over the hill.")
+elif age == 21:
+    print("Congrats on your 21st")
+else:
+    print("age is just a number.")
+        
